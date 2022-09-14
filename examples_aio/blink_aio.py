85b260e1894ba9132f9fea6353f7964c003dd6b8
MrYsLab <MisterYsLab@gmail.com>
2022-09-14 15:13:14 -0400

    renamed file

---
+++ examples_aio/blink_aio.py
@@ -0,0 +1,66 @@
+"""
+ Copyright (c) 2021 Alan Yorinks All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+import asyncio
+import sys
+
+from telemetrix_rpi_pico_w_aio import telemetrix_rpi_pico_w_aio
+
+"""
+Setup a pin for digital output and output a signal
+and toggle the pin. Do this 4 times.
+"""
+
+# some globals
+DIGITAL_PIN = 32  # arduino pin number
+
+
+async def blink(my_board, pin):
+    """
+    This function toggles a digital pin.
+
+    :param my_board: telemetrix instance
+    :param pin: pin to be controlled
+    """
+
+    # set the pin mode
+    await my_board.set_pin_mode_digital_output(pin)
+
+    # toggle the pin 4 times and exit
+    for x in range(4):
+        print('ON')
+        await my_board.digital_write(pin, 0)
+        await asyncio.sleep(1)
+        print('OFF')
+        await my_board.digital_write(pin, 1)
+        await asyncio.sleep(1)
+
+
+# get the event loop
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+# instantiate tmx_nano2040_wifi_aio
+board = telemetrix_rpi_pico_w_aio.TelemetrixRpiPicoWAio(ip_address='192.168.2.102')
+
+try:
+    # start the main function
+    loop.run_until_complete(blink(board, DIGITAL_PIN))
+    loop.run_until_complete(board.shutdown())
+
+except KeyboardInterrupt:
+    loop.run_until_complete(board.shutdown())
+    sys.exit(0)
