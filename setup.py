be92490f286059797757d4add09c80eaa2c8e4a6
MrYsLab <MisterYsLab@gmail.com>
2022-09-21 16:14:28 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ setup(
     install_requires=['pyserial'],
 
     version='1.0',
-    description="Remotely Control And Monitor A Raspberry Pi Pico",
+    description="Remotely Control And Monitor A Raspberry Pi Pico W",
     long_description=long_description,
     long_description_content_type='text/markdown',
 
     author='Alan Yorinks',
     author_email='MisterYsLab@gmail.com',
-    url='https://github.com/MrYsLab/telemetrix-rpi-pico',
-    download_url='https://github.com/MrYsLab/telemetrix-rpi-pico',
-    keywords=['telemetrix', 'Raspberry_Pi_Pico', 'Protocol', 'Python'],
+    url='https://github.com/MrYsLab/telemetrix-rpi-pico-w',
+    download_url='https://github.com/MrYsLab/telemetrix-rpi-pico-w',
+    keywords=['telemetrix', 'Raspberry_Pi_Pico_W', 'Protocol', 'Python'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
-
